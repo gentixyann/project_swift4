2f06b095d0d17452d3604242768cca5e89a04834
Gento <a551.return@gmail.com>
2018-03-23 16:15:18 +0900

    検索したところにピンが立つ

--- Map/Map/ViewController.swift
+++ Map/Map/ViewController.swift
@@ -37,6 +37,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
             if let placemark = placemarks?[0]{
                 if let targetCoordinate = placemark.location?.coordinate{
                     print(targetCoordinate)
+                    
+                    let pin = MKPointAnnotation()
+                    pin.coordinate = targetCoordinate
+                    pin.title = searchKeyword
+                    
+                    self.displayMap.addAnnotation(pin)
+                    self.displayMap.region =
+                    MKCoordinateRegionMakeWithDistance(targetCoordinate, 500.0, 500.0)
                 }
             }
         })
