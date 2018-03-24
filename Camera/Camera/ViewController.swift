2a7a79781f42dfbb2814788b6abfe4ff1a9a9c37
Gento <a551.return@gmail.com>
2018-03-24 15:15:38 +0900

    撮影後の処理

--- Camera/Camera/ViewController.swift
+++ Camera/Camera/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var photoImage: UIImageView!
+    
+    @IBAction func cameraLaunchAction(_ sender: Any) {
+        
+        //.isSourceTypeAvailableこの場合、カメラが使えるかの確認
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            print("camera can be used.")
+            
+            let ipc = UIImagePickerController()
+            ipc.sourceType = .camera
+            ipc.delegate = self
+            present(ipc, animated: true, completion: nil)
+            
+        }else{
+            print("camera is not available.")
+        }
+    }
+    
+    
+    @IBAction func shareAction(_ sender: Any) {
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        photoImage.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        dismiss(animated: true, completion: nil)
+    }
 }
 
