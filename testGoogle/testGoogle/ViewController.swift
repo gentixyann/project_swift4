b092fd66ccc178282e02aa5e9438e5dd1df63be5
Gento <a551.return@gmail.com>
2018-03-27 21:05:16 +0900

    start google map

---
+++ testGoogle/testGoogle/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testGoogle
+//
+//  Created by げんと on 2018/03/27.
+//  Copyright © 2018年 Gento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
