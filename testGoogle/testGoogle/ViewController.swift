60c763b348b350be54390be569fd72d608d59546
Gento <a551.return@gmail.com>
2018-03-28 18:24:33 +0900

    Mainstoryboardを使っての表示

--- testGoogle/testGoogle/ViewController.swift
+++ testGoogle/testGoogle/ViewController.swift
@@ -12,36 +12,9 @@ import GooglePlaces
 
 class ViewController: UIViewController {
     
-    var googleMap : GMSMapView!
-    
-    //緯度経度 -> 金沢駅
-    let latitude: CLLocationDegrees = 36.5780574
-    let longitude: CLLocationDegrees = 136.6486596
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        // ズームレベル.
-        let zoom: Float = 15
-        
-        // カメラを生成.
-        let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: latitude,longitude: longitude, zoom: zoom)
-        
-        // MapViewを生成.
-        googleMap = GMSMapView(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
-        
-        // MapViewにカメラを追加.
-        googleMap.camera = camera
-        
-        //マーカーの作成
-        let marker: GMSMarker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(latitude, longitude)
-        marker.map = googleMap
-        
-        
-        //viewにMapViewを追加.
-        self.view.addSubview(googleMap)
         
     }
     
@@ -51,5 +24,7 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBOutlet weak var mapView: GMSMapView!
+    
 }
 
