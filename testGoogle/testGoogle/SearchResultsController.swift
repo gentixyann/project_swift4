2a2ed00828f5b8840ca9cfcd03014e5921f36918
Gento <a551.return@gmail.com>
2018-03-29 00:05:05 +0900

    リセット

---
+++ testGoogle/testGoogle/SearchResultsController.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResultsControllerTableViewController.swift
+//  testGoogle
+//
+//  Created by げんと on 2018/03/28.
+//  Copyright © 2018年 Gento. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+import GooglePlaces
+import GooglePlacePicker
+
+class SearchResultsController: UITableViewController {
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+}
+}
+
--- testGoogle/testGoogle/ViewController.swift
+++ testGoogle/testGoogle/ViewController.swift
@@ -9,34 +9,84 @@
 import UIKit
 import GoogleMaps
 import GooglePlaces
+import GooglePlacePicker
 
-class ViewController: UIViewController, UISearchBarDelegate {
+
+class ViewController: UIViewController {
+    
+    var placesClient: GMSPlacesClient!
+    
+    // Add a pair of UILabels in Interface Builder, and connect the outlets to these variables.
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        placesClient = GMSPlacesClient.shared()
+    }
+    
+    // Add a UIButton in Interface Builder, and connect the action to this function.
+    @IBAction func getCurrentPlace(_ sender: UIButton) {
         
+        placesClient.currentPlace(callback: { (placeLikelihoodList, error) -> Void in
+            if let error = error {
+                print("Pick Place error: \(error.localizedDescription)")
+                return
+            }
+            
+            self.nameLabel.text = "No current place"
+            self.addressLabel.text = ""
+            
+            if let placeLikelihoodList = placeLikelihoodList {
+                let place = placeLikelihoodList.likelihoods.first?.place
+                if let place = place {
+                    self.nameLabel.text = place.name
+                    self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ")
+                        .joined(separator: "\n")
+                }
+            }
+        })
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    override func loadView() {
-        let camera = GMSCameraPosition.camera(withLatitude:1.285, longitude:103.848, zoom:12)
-        let mapView = GMSMapView.map(withFrame: .zero, camera: camera)
-        self.view = mapView
+    // Present the Autocomplete view controller when the button is pressed.
+    @IBAction func autocompleteClicked(_ sender: UIButton) {
+        let autocompleteController = GMSAutocompleteViewController()
+        autocompleteController.delegate = self
+        present(autocompleteController, animated: true, completion: nil)
     }
     
     @IBOutlet weak var mapView: GMSMapView!
     
+}
+
+
+extension ViewController: GMSAutocompleteViewControllerDelegate {
     
-    @IBAction func searchWithAddress(_ sender: Any) {
-        let searchController = UISearchController(searchResultsController: nil)
-        searchController.searchBar.delegate = self
-        self.present(searchController, animated:true, completion: nil)
+    // Handle the user's selection.
+    func viewController(_ viewController: GMSAutocompleteViewController, didAutocompleteWith place: GMSPlace) {
+        print("Place name: \(place.name)")
+        print("Place address: \(String(describing: place.formattedAddress))")
+        print("Place attributions: \(String(describing: place.attributions))")
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func viewController(_ viewController: GMSAutocompleteViewController, didFailAutocompleteWithError error: Error) {
+        // TODO: handle the error.
+        print("Error: ", error.localizedDescription)
+    }
+    
+    // User canceled the operation.
+    func wasCancelled(_ viewController: GMSAutocompleteViewController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    // Turn the network activity indicator on and off again.
+    private func DidRequestAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    private func DidUpdateAutocompletePredictions(_ viewController: GMSAutocompleteViewController) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
     
 }
-
