2a30f79580b38d9bcb78a26295ba7864ea93107b
Gento <a551.return@gmail.com>
2018-03-30 19:58:57 +0900

    google map can search address

---
+++ googletest/Pods/Target Support Files/Pods-googleMapiOS/Pods-googleMapiOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_googleMapiOS : NSObject
+@end
+@implementation PodsDummy_Pods_googleMapiOS
+@end
--- testGoogle/Pods/Target Support Files/Pods-testGoogle/Pods-testGoogle-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_testGoogle : NSObject
-@end
-@implementation PodsDummy_Pods_testGoogle
-@end
