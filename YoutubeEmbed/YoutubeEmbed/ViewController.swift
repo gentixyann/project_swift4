013916d0e31757da4d93e038e102b93031e6bdf8
Gento <a551.return@gmail.com>
2018-04-01 12:28:16 +0900

    This youtube type is webview.

---
+++ YoutubeEmbed/YoutubeEmbed/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  YoutubeEmbed
+//
+//  Created by げんと on 2018/04/01.
+//  Copyright © 2018年 Gento. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myWebView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        getVideo(videoCode: "OVGbAFy36xM")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func getVideo(videoCode: String){
+        let url = URL(string: "https://www.youtube.com/embed/\(videoCode)")
+        myWebView.loadRequest(URLRequest(url: url!))
+    }
+    
+    
+    
+}
+
